c7e59a147fdef63a1ac337a6007b52b69587060e
PaddyMcLoughlin-STUD <PaddyMcLoughlin-STUD@LAB-B2306-11>
2024-02-13 16:40:04 +0000

    Add project files.

---
+++ Worksheet 4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Worksheet_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "E12345";
+            IsValidEmployeeID(input);
+        }
+        static bool IsValidEmployeeID (string input)
+        {
+
+        }
+    }
+}
